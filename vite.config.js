8ed308736cb0dfb4a8c255026d06fcc12e74094c
George K. J <georgejo1012@gmail.com>
2026-04-26 17:40:57 +0530

    >>>

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// For user/organization GitHub Pages sites, base should be '/'
-// For project sites, base should be '/{repo-name}/'
-const base = process.env.GITHUB_ACTIONS ? '/' : '/'
-
-// https://vite.dev/config/
+// Since your repo name is "portfolio"
 export default defineConfig({
-  base,
+  base: '/portfolio/',
   plugins: [react()],
 })
